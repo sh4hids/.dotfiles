5914934726c4f8953fee1d7ac364001be41ac16e
sh4hids <dev@shahid.pro>
2021-11-16 10:18:15 +0600

    ➕ add wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require 'wezterm';
+
+
+print("Emoji 🎉")
+
+return {
+  font = wezterm.font("FiraCode Nerd Font Mono"),
+  color_scheme = "Blazer",
+  font_size = 14,
+  window_background_opacity = 0.96,
+}
+
+
