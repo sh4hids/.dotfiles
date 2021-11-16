26be8d6c88bbfb6ce0a33d0e45cac835c0c579c1
sh4hids <dev@shahid.pro>
2021-11-16 10:22:15 +0600

    ↕ update wezterm theme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,11 +1,8 @@
 local wezterm = require 'wezterm';
 
-
-print("Emoji 🎉")
-
 return {
   font = wezterm.font("FiraCode Nerd Font Mono"),
-  color_scheme = "Blazer",
+  color_scheme = "MaterialOcean",
   font_size = 14,
   window_background_opacity = 0.96,
 }
