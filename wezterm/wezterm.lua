8579e59f48c8fcbc77a70399f4d54fc6da254abd
Shahidul Islam Majumder <dev@shahid.pro>
2021-12-23 06:24:12 +0600

    ↕ update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,7 +4,7 @@ return {
   font = wezterm.font("FiraCode Nerd Font Mono"),
   color_scheme = "MaterialOcean",
   font_size = 16,
-  window_background_opacity = 0.99,
+  window_background_opacity = 1.0,
 }
 
 
