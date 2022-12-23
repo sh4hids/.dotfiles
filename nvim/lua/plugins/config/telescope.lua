b5e2589d6d936be3a9eddf59b23e95303ba4a2ff
Shahidul Islam Majumder <dev@shahid.pro>
2022-12-23 08:48:50 +0600

    fix: telescope config

--- nvim/lua/plugins/config/telescope.lua
+++ nvim/lua/plugins/config/telescope.lua
@@ -1,5 +1,23 @@
-require('telescope').setup()
 local builtin = require('telescope.builtin')
+local actions = require('telescope.actions')
+
+require('telescope').setup {
+  defaults = {
+    mappings = {
+      n = {
+        ["q"] = actions.close
+      },
+    },
+    file_ignore_patterns = {
+      "node_modules/.*",
+      ".git",
+      ".cache",
+      "dist",
+      "build",
+      "public",
+    },
+  }
+}
 
 vim.keymap.set('n', 'ff', builtin.find_files, {})
 vim.keymap.set('n', 'fo', builtin.oldfiles, {})
