893497119ed52abb46883bba5774f51a8c24ccec
Shahidul Islam Majumder <dev@shahid.pro>
2023-01-27 07:27:23 +0600

    feat: add vim-fugitive

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -229,6 +229,11 @@ _G.packer_plugins = {
     path = "/Users/shahid/.local/share/nvim/site/pack/packer/start/toggleterm.nvim",
     url = "https://github.com/akinsho/toggleterm.nvim"
   },
+  ["vim-fugitive"] = {
+    loaded = true,
+    path = "/Users/shahid/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    url = "https://github.com/tpope/vim-fugitive"
+  },
   ["vim-hexokinase"] = {
     loaded = true,
     path = "/Users/shahid/.local/share/nvim/site/pack/packer/start/vim-hexokinase",
