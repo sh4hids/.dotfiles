795c9a85dcaeaff336f166a27725bf1124e0c438
Shahidul Islam Majumder <dev@shahid.pro>
2024-01-11 13:00:40 +0600

    feat: add a log helper plugin

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -4,4 +4,49 @@ return {
     cmd = "IncRename",
     config = true,
   },
+  {
+    "chrisgrieser/nvim-chainsaw",
+    opts = {
+      marker = "🚧",
+    },
+    keys = {
+      {
+        "<leader>ms",
+        function()
+          require("chainsaw").messageLog()
+        end,
+        desc = "󰸢 message log",
+      },
+      {
+        "<leader>mv",
+        function()
+          require("chainsaw").variableLog()
+        end,
+        mode = { "n", "x" },
+        desc = "󰸢 variable log",
+      },
+      {
+        "<leader>mo",
+        function()
+          require("chainsaw").objectLog()
+        end,
+        mode = { "n", "x" },
+        desc = "󰸢 object log",
+      },
+      {
+        "<leader>mt",
+        function()
+          require("chainsaw").timeLog()
+        end,
+        desc = "󰸢 time log",
+      },
+      {
+        "<leader>mr",
+        function()
+          require("chainsaw").removeLogs()
+        end,
+        desc = "󰹝 remove logs",
+      },
+    },
+  },
 }
