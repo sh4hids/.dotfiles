68fa2e4ebe94314767af215595c7f7b11594bf4f
Shahidul Islam Majumder <dev@shahid.pro>
2024-11-26 08:28:55 +0600

    fix: refactor some config

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.del("t", "<c-j>")
+
+-- vim.keymap.del("t", "<c-j>")
 
 vim.keymap.set("n", "<C-/>", function()
   local Util = require("lazyvim.util")
