2e0341b8329129d0d807ed4545efc7fd7e4589b8
Shahidul Islam Majumder <dev@shahid.pro>
2025-08-28 08:25:55 +0600

    feat: add window border

---
+++ .config/aerospace/musaeid.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+current_workspace=$(aerospace list-workspaces --focused)
+
+aerospace list-windows --all | grep -E "(Musaeid|musaeid)" | awk '{print $1}' | while read window_id; do
+  if [ -n "$window_id" ]; then
+    aerospace move-node-to-workspace --window-id "$window_id" "$current_workspace"
+  fi
+done
