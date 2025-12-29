8126fca35cd595962ce28b3250af591077ce450c
Shahidul Islam Majumder <dev@shahid.pro>
2025-12-29 20:44:51 +0600

    fix: wezterm tab bar view

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,7 +5,6 @@ local config = wezterm.config_builder()
 config.font_size = 20
 config.color_scheme = 'zenbones'
 config.use_fancy_tab_bar = false
-config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = 'INTEGRATED_BUTTONS | RESIZE'
 config.enable_scroll_bar = false
 config.window_padding = {
