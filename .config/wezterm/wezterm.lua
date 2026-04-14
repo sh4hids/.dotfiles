1ba5e123a2376081d061bfab63e937ed6897e6de
Shahidul Islam Majumder <dev@shahid.pro>
2026-04-14 20:14:22 +0600

    fix: update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,16 +3,22 @@ local wezterm = require('wezterm')
 local config = wezterm.config_builder()
 
 config.font_size = 20
+config.line_height = 1.2
 config.color_scheme = 'zenbones'
 config.use_fancy_tab_bar = false
-config.window_decorations = 'INTEGRATED_BUTTONS | RESIZE'
+config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
+config.enable_tab_bar = false
 config.enable_scroll_bar = false
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 20,
-  bottom = 0,
+  left = 24,
+  right = 24,
+  top = 60,
+  bottom = 24,
 }
 config.font = wezterm.font('RecMonoSmCasual Nerd Font')
+config.window_background_opacity = 0.4
+config.text_background_opacity = 0.4
+config.macos_window_background_blur = 40
+-- config.window_background_image = '/Users/shahid/.dotfiles/assets/wallpapers/bg-blurred-2.png'
 
 return config
