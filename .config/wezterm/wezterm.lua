a9c1171d21afc285cd8f0c279dc23f7b5cf76119
Shahidul Islam Majumder <dev@shahid.pro>
2026-09-03 08:35:13 +0600

    feat: refactor wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,14 +1,7 @@
 local wezterm = require('wezterm')
 
 local config = wezterm.config_builder()
-
-local function get_scheme_by_appearance()
-  if wezterm.gui.get_appearance():find('Dark') then
-    return 'kanagawabones'
-  else
-    return 'rose-pine-dawn'
-  end
-end
+local is_in_dark_mode = wezterm.gui.get_appearance():find('Dark')
 
 config.font_size = 20
 config.line_height = 1.2
@@ -16,18 +9,25 @@ config.use_fancy_tab_bar = false
 config.window_decorations = 'RESIZE'
 config.hide_tab_bar_if_only_one_tab = true
 config.enable_scroll_bar = false
+
 config.window_padding = {
   left = 24,
   right = 24,
   top = 60,
   bottom = 24,
 }
+
 config.font = wezterm.font('RecMonoSmCasual Nerd Font')
 config.macos_window_background_blur = 40
 
-config.color_scheme = get_scheme_by_appearance()
-
-config.window_background_opacity = 0.4
-config.text_background_opacity = 0.4
+if is_in_dark_mode then
+  config.color_scheme = 'kanagawabones'
+  config.window_background_opacity = 0.4
+  config.text_background_opacity = 0.4
+else
+  config.color_scheme = 'rose-pine-dawn'
+  config.window_background_opacity = 0.6
+  config.text_background_opacity = 0.6
+end
 
 return config
